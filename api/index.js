eaf480f9972d94a1b3285849ef3b7438bb65e299
sophiediiorio <sophiediiorio@gmail.com>
2025-08-21 21:37:40 -0400

    Updates to API service

--- api/index.js
+++ api/index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Simple Hello World endpoint
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
+app.get('/', (_, res) => res.json({ message: 'Hello World' }));
 
 app.listen(port, () => {
   console.log(`🚀 API running on port ${port}`);
